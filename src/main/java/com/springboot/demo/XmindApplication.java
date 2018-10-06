efce1304bae8371203a1fa94e88fc81e30bfcbf3
jwwam <jwwam@users.noreply.github.com>
2018-10-06 08:42:06 +0800

    Initial commit

---
+++ src/main/java/com/springboot/demo/XmindApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class XmindApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmindApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		System.out.println("外部tomcat,chapter启动!");
+		return application.sources(XmindApplication.class);
+	}
+
+}
